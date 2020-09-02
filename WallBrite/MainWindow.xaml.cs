6d910e951b5dd55584c3ed20febab5766a10d9e2
bradleythebaker <dragunzor@gmail.com>
2020-09-02 03:45:27 -0400

    Added folder selection (rather than just direct file selection)

--- WallBrite/MainWindow.xaml.cs
+++ WallBrite/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using Microsoft.Win32;
-using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WinForms = System.Windows.Forms;
 
 namespace WallBrite
 {
@@ -27,14 +17,13 @@ namespace WallBrite
             InitializeComponent();
         }
 
-        private void Test_Click(object sender, RoutedEventArgs e)
+        private void Add_Files(object sender, RoutedEventArgs e)
         {
             // Create OpenFileDialog to browse files
             OpenFileDialog dialog = new OpenFileDialog
             {
-
                 // Filter dialog to only show supported image types (or all files)
-                Filter = "Images|*.jpg; *.png; *.gif; *.bmp; *.exif; *.tiff" +
+                Filter = "Images|*.jpg; *.jpeg; *.png; *.gif; *.bmp; *.exif; *.tiff" +
                             "|All Files|*.*",
 
                 // Set dialog to select multiple files
@@ -42,16 +31,55 @@ namespace WallBrite
             };
 
             // If user clicked OK (not Cancel) in file dialog
-            if (dialog.ShowDialog() == true) {
-                // Create stream from selected file
+            if (dialog.ShowDialog() == true)
+            {
+                // TODO: add try catch for possible exceptions
+                // Create streams from selected files
                 Stream[] fileStreams = dialog.OpenFiles();
 
-                // 
+                //
                 foreach (Stream stream in fileStreams)
                 {
                     WBImage image = new WBImage(stream);
                 }
-                
+            }
+        }
+
+        private void Add_Folder(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog to browse files
+            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+
+            // If user clicked OK (not Cancel) in folder dialog
+            if (dialog.ShowDialog() == WinForms.DialogResult.OK)
+            {
+                // Create stream from selected file
+                string folderPath = dialog.SelectedPath;
+
+                // TODO: add try catch for possible exceptions of getfiles; pathtoolong??
+                // Get paths of all supported image files in this folder and subfolders
+                IEnumerable<string> filePaths = Directory.EnumerateFiles(folderPath,
+                                                    "*.*",
+                                                    SearchOption.AllDirectories)
+                                     .Where(filePath => filePath.EndsWith(".jpg")
+                                                        || filePath.EndsWith(".jpeg")
+                                                        || filePath.EndsWith(".png")
+                                                        || filePath.EndsWith(".gif")
+                                                        || filePath.EndsWith(".bmp")
+                                                        || filePath.EndsWith(".exif")
+                                                        || filePath.EndsWith(".tiff"));
+
+                // Loop over each filePath in the array of filePaths (over each file in selected folder and
+                // all subfolders)
+                foreach (string filePath in filePaths)
+                {
+                    // TODO: add try catch for possible exceptions
+                    // Open file stream for file at this path
+                    FileStream stream = File.Open(filePath, FileMode.Open);
+
+                    // Create WBImage for that file
+                    WBImage image = new WBImage(stream);
+                }
             }
         }
     }
