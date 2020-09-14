62e6fb4f8791f2a836e1493fd04e687000011df0
bradleythebaker <dragunzor@gmail.com>
2020-09-13 22:37:01 -0400

    Added skeleton of front-end for image library; resizable grid working!

--- WallBrite/MainWindow.xaml.cs
+++ WallBrite/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace WallBrite
 {
@@ -13,16 +17,107 @@ namespace WallBrite
         public MainWindow()
         {
             InitializeComponent();
+            this.SizeChanged += OnWindowSizeChange;
+        }
+        
+
+        private void OnWindowSizeChange(object sender, SizeChangedEventArgs e)
+        {
+            RefreshImageGrid();
         }
 
         private void AddFiles(object sender, RoutedEventArgs e)
         {
             WBManager.AddFiles();
+            RefreshImageGrid();
         }
 
         private void AddFolder(object sender, RoutedEventArgs e)
         {
             WBManager.AddFolder();
+            RefreshImageGrid();
         }
+
+        // TODO: move refresh code to a place that makes more sense
+        private void RefreshImageGrid ()
+        {
+            // Clear grid of images before repopulating
+            imageGrid.Children.Clear();
+            imageGrid.RowDefinitions.Clear();
+            imageGrid.ColumnDefinitions.Clear();
+
+            // Create first row before populating
+            RowDefinition firstRow = new RowDefinition
+            {
+                Height = new GridLength(200)
+            };
+            imageGrid.RowDefinitions.Add(firstRow);
+
+            // Maintain count of current row and column in grid for image placement
+            int row = 0;
+            int column = 0;
+
+            double availableGridWidth = MainPanel.ActualWidth - SidePanel.ActualWidth;
+            // Total width currently taken up by all grid columns; for use in resizing grid to fit window
+            int totalWidth = 0;
+
+            // Loop over each image in the library
+            foreach (WBImage wbImage in WBLibrary.LibraryList)
+            {
+                // Create image to be shown in library grid; set source to thumbnail for this WBImage
+                Image gridImage = new Image();
+                BitmapImage sourceBitmap = WBHelpers.ImagetoBitmapSource(wbImage.Thumbnail);
+                gridImage.Source = sourceBitmap;
+
+                // Add image to grid
+                imageGrid.Children.Add(gridImage);
+                
+
+                // If placing this image in next column will fit in window, then place it there
+                if (totalWidth + 200 <= availableGridWidth)
+                {
+                    // If this is first row, column still needs to be created, so create it
+                    if (row == 0)
+                    {
+                        ColumnDefinition newColumn = new ColumnDefinition
+                        {
+                            Width = new GridLength(200)
+                        };
+                        imageGrid.ColumnDefinitions.Add(newColumn);
+                    }
+
+                    // PLace image at this row and column
+                    Grid.SetRow(gridImage, row);
+                    Grid.SetColumn(gridImage, column);
+
+                    // Increment counters for next image (possibly) in this row
+                    column++;
+                    totalWidth += 200;
+                } 
+                // Otherwise place this image on a new row starting at column 0
+                else {
+                    // Create new row
+                    RowDefinition newRow = new RowDefinition
+                    {
+                        Height = new GridLength(200)
+                    };
+                    imageGrid.RowDefinitions.Add(newRow);
+ 
+                    // Increment row counter and reset column and width counters
+                    row++;
+                    column = 0;
+
+                    // Place image at this row and column
+                    Grid.SetRow(gridImage, row);
+                    Grid.SetColumn(gridImage, column);
+
+                    // Increment column and total width counters so next image is (possibly) placed in next
+                    // column
+                    column++;
+                    totalWidth = 200;
+                }
+            }
+        }
+        
     }
 }
---
+++ WallBrite/WBHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WallBrite
+{
+    static class WBHelpers
+    {
+
+        /// <summary>
+        /// Converts System.Drawing Image to a System.Windows.Media.Imaging BitmapImage, for use as an image
+        /// source in the WPF
+        /// Mostly taken from:
+        /// https://stackoverflow.com/questions/41998142/converting-system-drawing-image-to-system-windows-media-imagesource-with-no-resu
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static BitmapImage ImagetoBitmapSource (Image image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, ImageFormat.Bmp);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = ms;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
